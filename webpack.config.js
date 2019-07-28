009e6d47fd867bc9e3b347d78b896c04871fbf33
KAHLAOUI Mathieu <mathieu.kahlaoui@oscaro.com>
2019-07-28 23:06:18 +0200

    minimum webpack configuration done

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // defining entry point of the bundle
+    entry:"./src/index.js",
+    // defining the output point of the bundle
+    output: {
+        filename: "bundle.js",
+        path: "./dist"
+    },
+    mode: "none"
+}
