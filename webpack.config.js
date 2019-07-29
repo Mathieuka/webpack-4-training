8a38c1ac7e87e102cd29e702306635a040b77b39
KAHLAOUI Mathieu <mathieu.kahlaoui@oscaro.com>
2019-07-29 22:19:36 +0200

    handling image with webpack and add public path done

--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,20 @@ module.exports = {
     // defining the output point of the bundle.
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "./dist") // Here we need pass absolute path.
+        path: path.resolve(__dirname, "./dist"), // Here we need pass absolute path.
+        publicPath: "dist/" // IMPORTANT: here we specify the public path, the public path tells where all the generated file are located.
+                            // example if the app is deploy the public path is http://mydomain.com/
     },
     mode: "none",
     module: {
         rules: [
-            {
+            {   
                 //Example => Every time we try to import a jpg file, Webpack will check if the rules is present.
                 test: /\.(png|jpg)$/,   // For add an image. 
                 use: [ // Here we specify wich loader should be used by webpack.
                      "file-loader"
                 ]
-            },
-            // { // xml rules is just an example.
-            //     test: /\.(xml)$/,
-            //     use: [
-            //         "xml-loader" 
-            //     ]
-            // }
+            }
         ]
     }
 }
