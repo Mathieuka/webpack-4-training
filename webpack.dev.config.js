3c8df07f6a0b88abd8b2390de82d1ff469adca33
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2019-11-21 21:45:08 +0100

    set production and development webpack build configuration

---
+++ webpack.dev.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry:'./src/index.js',
+    // defining the output point of the bundle.
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'), // Here we need pass absolute path.
+        publicPath: '../' // IMPORTANT: here we specify the public path, the public path tells where all the generated file are located.
+                            // example if the app is deploy the public path is http://mydomain.com/
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                  }
+                }
+            },
+            {   
+                //Example => Every time we try to import a jpg file, Webpack will check if the rules is present.
+                test: /\.(png|jpg)$/,   // For add an image. 
+                use: [ // Here we specify wich loader should be used by webpack.
+                     'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*', // this means clean all file bundle in publicPath
+                path.join(process.cwd(), 'build_example/**/*')
+
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Hello people',
+            filename: 'subfolder/index_folder_customized.html',
+            meta: {
+                description: 'Some description'
+            }
+        })
+    ]
+}
---
+++ webpack.production.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Extract the css in specific style bundle
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry:'./src/index.js',
+    // defining the output point of the bundle.
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, './dist'), // Here we need pass absolute path.
+        publicPath: '../' // IMPORTANT: here we specify the public path, the public path tells where all the generated file are located.
+                            // example if the app is deploy the public path is http://mydomain.com/
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                  }
+                }
+            },
+            {   
+                //Example => Every time we try to import a jpg file, Webpack will check if the rules is present.
+                test: /\.(png|jpg)$/,   // For add an image. 
+                use: [ // Here we specify wich loader should be used by webpack.
+                     'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: 'style.[contenthash].css',
+            // chunkFilename: '[id].css',
+            // ignoreOrder: false, // Enable to remove warnings about conflicting order
+          }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*', // this means clean all file bundle in publicPath
+                path.join(process.cwd(), 'build_example/**/*')
+
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Hello people',
+            filename: 'subfolder/index_folder_customized.html',
+            meta: {
+                description: 'Some description'
+            }
+        })
+    ]
+ }
+
+
+
